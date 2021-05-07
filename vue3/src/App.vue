fbc89360d12a2990bad920062ce4b61dc790ffc5
v_hyahu <v_hyahu@tencent.com>
2021-05-07 15:35:00 +0800

    fix:vue3.0项目搭建

---
+++ vue3/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <a-layout class="layout">
+    <a-layout-header>
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          <router-link to="/">首页</router-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <router-link to="/plan">时间计划</router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+        <router-view></router-view>
+      </div>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
+</template>
+<script>
+import {reactive,toRefs} from 'vue'
+export default {
+  setup(props,context) {
+    console.log(props,context)
+    const state = reactive({
+      selectedKeys:['2']
+    })
+    return {
+      ...toRefs(state)
+    };
+  },
+};
+</script>
+<style>
+.site-layout-content {
+  min-height: 280px;
+  padding: 24px;
+  background: #fff;
+}
+#components-layout-demo-top .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+.ant-row-rtl #components-layout-demo-top .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+[data-theme='dark'] .site-layout-content {
+  background: #141414;
+}
+</style>
+
---
+++ vue3/src/views/Plan/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        时间计划
+    </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    };
+  }
+}
+</script>
+<style lang="css" scoped>
+</style>
